03b5e7fafe2ef6b9cda43d0ba102010a7e3d5312
Denys Chornyi <chornijj14@gmail.com>
2020-11-13 01:48:18 +0100

    Make own redirect based on location and history, if don't get any location.query redirect to main page

--- src/pages/Results/Results.js
+++ src/pages/Results/Results.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./results.css";
 
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import { Container } from "reactstrap";
 
 export function Results() {
   const location = useLocation();
-  console.log(location.query);
+  const history = useHistory();
+
+  const query = location.query;
+
+  if (!query) {
+    history.push("/");
+  }
 
   return (
     <div>
